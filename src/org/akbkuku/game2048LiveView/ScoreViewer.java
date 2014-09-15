1188c8c1e1570ca009e37b491d6da2d8e11c9054
AkBKukU <shelby@jueden.org>
2014-09-14 23:39:33 -0700

    Score display working

--- src/org/akbkuku/game2048LiveView/ScoreViewer.java
+++ src/org/akbkuku/game2048LiveView/ScoreViewer.java
@@ -1,24 +1,31 @@
 package org.akbkuku.game2048LiveView;
 
+import com.sonyericsson.extras.liveview.plugins.PluginConstants;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.util.Log;
 
 public class ScoreViewer extends LiveViewActivity{
-
+ 
 	@Override
 	public void buildImage() {
+	    Log.d(PluginConstants.LOG_TAG_GAME, "Displaying Score");		
+		Bitmap background = BitmapFactory.decodeStream(context.getResources().openRawResource(R.drawable.background));
+		// Print Background
+		canvas.drawBitmap(background, 0, 0,null);
 
-	    Paint paint = new Paint();
+	    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-		paint.setColor(Color.BLACK);
-		paint.setStyle(Style.FILL); 
-		canvas.drawPaint(paint); 
+	    
 		paint.setColor(Color.WHITE); 
 		paint.setTextSize(20); 
 	    
 		// TODO Auto-generated method stub
-		canvas.drawText("Score: "+SandboxPluginService.score, 0, 0, paint);
+		canvas.drawText("Score: "+SandboxPluginService.score, 0, 20, paint);
 	}
 
 }
